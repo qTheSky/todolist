b4d8b56ee868d3f7f42a5a0b767c7741a50885f4
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-04-28 12:58:07 +0300

    objects lesson

---
+++ js-lessons/js-lessons/src/02-objects/02.ts
@@ -0,0 +1,50 @@
+type LocalCityType = {
+    title: string
+    country: string
+}
+type AdressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+type TechType = {
+    id: number
+    title: string
+}
+type StudentType= {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address: AdressType
+    technologies: Array<TechType>
+}
+
+const students: StudentType = {
+    id: 1,
+    name: 'Dimych',
+    age: 32,
+    isActive: false,
+    address: {
+        streetTitle: 'Surganova 2',
+        city: {
+            title: 'Minsk',
+            country: 'Belarus'
+        }
+    },
+    technologies:[
+        {
+            id: 1,
+            title: "HTML"
+        },
+        {
+            id: 2,
+            title: "CSS"
+        },{
+            id: 3,
+            title: "REACT"
+        },
+    ]
+
+}
+
+console.log(students.technologies[2].title)
