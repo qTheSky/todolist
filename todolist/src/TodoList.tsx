3a0892da94770a83759688963efe34c74cc2bedb
Mikhail Smirnov <smirnov.mic@yandex.ru>
2022-05-02 21:20:32 +0300

    2 lesson lastStep
    2 lesson lastStep

--- todolist/src/TodoList.tsx
+++ todolist/src/TodoList.tsx
@@ -18,22 +18,27 @@ export type TaskType = {
 const TodoList = (props: TodoListPropsType) => {
     let tasksForRender = props.tasks
     if (props.filter === 'active') {
-        tasksForRender = props.tasks.filter(t => t.isDone === false)
+        tasksForRender = props.tasks.filter(t => !t.isDone)
     }
     if (props.filter === 'completed') {
-        tasksForRender = props.tasks.filter(t => t.isDone === true)
+        tasksForRender = props.tasks.filter(t => t.isDone)
     }
 
-    const tasksJSXElements = tasksForRender.map(t => {
-        const removeTask = () => props.removeTask(t.id)
-        return (
-            <li key={t.id}>
-                <input type="checkbox" checked={t.isDone}/>
-                <span>{t.title}</span>
-                <button onClick={removeTask}>X</button>
-            </li>
-        )
-    })
+    const tasksJSXElements = tasksForRender.length
+        ? tasksForRender.map(t => {
+            const removeTask = () => props.removeTask(t.id)
+            return (
+                <li key={t.id}>
+                    <input type="checkbox" checked={t.isDone}/>
+                    <span>{t.title}</span>
+                    <button onClick={removeTask}>X</button>
+                </li>
+            )
+        })
+        : <span>List is empty</span>
+    const changeFilter = (filter: filterValueType) => {
+        return () => props.changeFilter(filter)
+    }
     return (
         <div>
             <h3>{props.title}</h3>
@@ -45,9 +50,9 @@ const TodoList = (props: TodoListPropsType) => {
                 {tasksJSXElements}
             </ul>
             <div>
-                <button onClick={()=>props.changeFilter('all')}>All</button>
-                <button onClick={()=>props.changeFilter('active')}>Active</button>
-                <button onClick={()=>props.changeFilter('completed')}>Completed</button>
+                <button onClick={changeFilter('all')}>All</button>
+                <button onClick={changeFilter('active')}>Active</button>
+                <button onClick={changeFilter('completed')}>Completed</button>
             </div>
         </div>
     );
